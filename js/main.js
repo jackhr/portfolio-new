8efd3fd5ad25401cd0d0cccc2bb0e7b59fc266dd
jackhr <jc2o@mac.com>
2023-03-13 14:41:49 -0400

    Change scrolling to be manual again

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 });
 
 let lastY;
-$('#intro, main').on('touchstart', function(e) {
+$('#intro').on('touchstart', function(e) {
   lastY = e.originalEvent.touches[0].clientY;
 }).on('mousewheel DOMMouseScroll touchmove', function(e) {
   
@@ -26,80 +26,30 @@ $('#intro, main').on('touchstart', function(e) {
 
   if (!mobileScroll && delta > threshold*-1 && delta < threshold) return;
 
-  if (this.id === 'intro') {
-    if (delta > 0 && $(this).scrollTop() === 0) {
-      // If scrolling up on intro then return
-      return e.preventDefault();
-    }
-
-    if (this.id === 'intro') {
-      // Slide up intro
-      $(this).removeClass('viewing');
-      setTimeout(() => {
-        $(this).remove();
-        // Make main scrollable
-        $("main").removeClass('no-scroll');
-
-        if (!STATE.startedWelcome) {
-          $(".intro-text-white").addClass('disappear');
-          
-          setTimeout(() => {
-            $(".intro-text-white").remove();
-            
-            initWelcomeType();
-          }, 2000);
-
-          STATE.startedWelcome = true;
-        }
-        
-      }, 1500);
-      return;
-    }
+  if (delta > 0 && $(this).scrollTop() === 0) {
+    // If scrolling up on intro then return
+    return e.preventDefault();
   }
 
-  // When scrolling on main
-  const main = this;
-  
-  // Let's not interfere while scrolling
-  if ($(main).hasClass('scrolling')) return;
-  $(main).addClass('scrolling');
-  // Get current section for reference
-  const initialSection = $(main).find('.viewing');
-
-  if (initialSection.hasClass('about') && delta < 0) {
-    // Can't scroll down if we are at the bottom
-    e.preventDefault();
-    return $(main).removeClass('scrolling');
-  } else if (initialSection.hasClass('first') && delta > 0) {
-    // Can't scroll up if we are at the top
-    e.preventDefault();
-    return $(main).removeClass('scrolling');
-  }
-
-  // Function to determine which sibling to scroll to
-  const getSibling = delta => delta > 0 ? 'prev' : 'next';
-  // Display next sectino to scroll to
-  initialSection[getSibling(delta)]().addClass('viewing');
-  // Process factor to scroll by
-  const factor = initialSection.hasClass('projects') ? delta > 0 ? 0 : -2 : -1;
-  // Make sure scroll value is maximum as possible to scroll fully
-  const top = $(initialSection).height() * factor;
-  // Keep current section in viewport
-  // Finally scroll to new section
-  $('main .section').css({
-    'transform': `translateY(${top}px)`,
-  });
-
+  $(this).removeClass('viewing');
   setTimeout(() => {
-    // When finished scrolling remove intial section from view
-    initialSection.removeClass('viewing');
-    // Allow user to scroll again
-    $(main).removeClass('scrolling');
-  }, 1200);
+    $(this).remove();
+    // Make main scrollable
+    $("main").removeClass('no-scroll');
 
+    if (!STATE.startedWelcome) {
+      // Only need to fire this once
+      STATE.startedWelcome = true;
+      $(".intro-text-white").addClass('disappear');
+      
+      setTimeout(() => {
+        $(".intro-text-white").remove();
+        initWelcomeType();
+      }, 750);
+    }
+  }, 1500);
   
 });
-
 
 function initWelcomeType() {
   new TypeIt("#welcome-text", { 
@@ -107,6 +57,9 @@ function initWelcomeType() {
     lifeLike: true,
     speed: 100,
     cursorChar: '<img src="assets/images/welcome-cursor.svg">',
-    afterComplete: async instance => instance.destroy()
+    afterComplete: async (instance) => {
+      instance.destroy();
+      $('html, body').removeClass('no-scroll');
+    }
   }).go();
 }
